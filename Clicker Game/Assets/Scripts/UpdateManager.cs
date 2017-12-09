6740260c536cfce8d08f06933124c071d07e3a1e
Chaitanya Jujjavarapu <chaitanya@Chaitanyas-MacBook-Pro.local>
2017-12-09 15:15:36 -0600

    clicker game 1

---
+++ Clicker Game/Assets/Scripts/UpdateManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateManager : MonoBehaviour {
+
+	public Click click;
+	public UnityEngine.UI.Text itemInfo;
+	public float cost;
+	public int count = 0;
+	public int clickPower;
+	public string itemName;
+	private float baseCount;
+
+	void Start() {
+		baseCost = cost;
+	}
+
+	void Update() {
+		itemInfo.text = itemName + "\nCost: " + cost + "\nPower: +" + clickPower;
+	
+	}
+
+	public void PurchasedUpgrade() {
+		if (click.gold >= cost) {
+			click.gold -= cost;
+			count += 1;
+			click.goldperclick += clickPower;
+			cost = Mathf.Round (baseCost * Mathf.Pow (1.15f, count));
+		}
+	}
+
+
+
+}
